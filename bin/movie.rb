e422aa219c68a52e59e0871a32b8060da985289f
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-07-16 22:26:18 +0300

    remove unnecessary code

--- bin/movie.rb
+++ bin/movie.rb
@@ -10,13 +10,4 @@ class Movie
     @title_for_movie = title_for_movie
     @price_code = price_code
   end
-
-  def test_initializer
-    puts @title_for_movie
-    puts @price_code
-  end
 end
-
-m = Movie.new(title_for_movie: "My Title", price_code: "2")
-
-m.test_initializer
