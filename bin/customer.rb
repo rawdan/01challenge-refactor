883851f36d93fb355972aba055988dec1bf3d7d5
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-07-16 22:27:47 +0300

    clean up test

--- bin/customer.rb
+++ bin/customer.rb
@@ -4,7 +4,7 @@ require_relative '../bin/movie'
 class Customer
   attr_accessor :customer_name
 
-  def initialize(customer_name)
+  def initialize(customer_name: customer_name)
     @customer_name = customer_name
     @rentals ||= []
   end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe Customer do
 
   it 'can initialize with a customer name' do
     customer = Customer.new(customer_name: "Robert")
-    expect(customer.customer_name).to eq({:customer_name=>"Robert"})
+    expect(customer.customer_name).to eq("Robert")
     end
 end
