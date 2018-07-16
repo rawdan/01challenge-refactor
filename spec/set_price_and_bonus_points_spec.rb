f05e6b33545aaf01eb318fed87c7ebcb45c5aab9
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-07-17 02:28:17 +0300

    add test for regular movie rented for 4 days

--- spec/set_price_and_bonus_points_spec.rb
+++ spec/set_price_and_bonus_points_spec.rb
@@ -5,13 +5,13 @@ require_relative '../bin/movie'
 require_relative '../bin/customer'
 
 RSpec.describe Interactors::SetPriceAndBonusPoints do
-  let(:new_movie)    {Movie.new(title_for_movie: "Jaws", price_code: Movie::NEW_RELEASE) }
-  let(:kids_movie)   {Movie.new(title_for_movie: "Incredibles 2", price_code: Movie::CHILDRENS_MOVIE)}
+  let(:new_movie)       {Movie.new(title_for_movie: "Jaws", price_code: Movie::NEW_RELEASE) }
+  let(:kids_movie)      {Movie.new(title_for_movie: "Incredibles 2", price_code: Movie::CHILDRENS_MOVIE)}
   let(:regular_movie)   {Movie.new(title_for_movie: "Avengers", price_code: Movie::REGULAR)}
-  let(:new_rental)   {Rental.new(movie: new_movie, days_rented: 2)}
-  let(:kids_rental)  {Rental.new(movie: kids_movie, days_rented: 2)}
-  let(:regular_rental)  {Rental.new(movie: regular_movie, days_rented: 3)}
-  let(:customer) {Customer.new(customer_name: "Robert")}
+  let(:new_rental)      {Rental.new(movie: new_movie, days_rented: 2)}
+  let(:kids_rental)     {Rental.new(movie: kids_movie, days_rented: 2)}
+  let(:regular_rental)  {Rental.new(movie: regular_movie, days_rented: 4)}
+  let(:customer)        {Customer.new(customer_name: "Robert")}
 
   describe 'calculates and prints out a statement' do
     it 'can calculate a price and bonus points awarded for a new realease movie rented for 2 days' do
@@ -20,6 +20,10 @@ RSpec.describe Interactors::SetPriceAndBonusPoints do
 
     it 'can calculate a price and bonus points awarded for a kids movie rented for 2 days' do
       expect(Interactors::SetPriceAndBonusPoints.call(kids_rental, customer)).to eq "Rental record for Robert\nAmount owed is $1.5\nYou earned 1 frequent renter points"
+    end
+
+    it 'can calculate a price and bonus points awarded for a regular movie rented for 4 days' do
+      expect(Interactors::SetPriceAndBonusPoints.call(regular_rental, customer)).to eq "Rental record for Robert\nAmount owed is $3.0\nYou earned 1 frequent renter points"
     end
   end
 end
