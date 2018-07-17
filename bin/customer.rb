122e2e35e0327db87a80fd366b6f870e6298f99b
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-07-17 17:03:45 +0300

    make tests pass

--- bin/customer.rb
+++ bin/customer.rb
@@ -1,7 +1,3 @@
-require_relative '../bin/rental'
-require_relative '../bin/movie'
-require_relative '../interactors/set_price_and_bonus_points'
-
 class Customer
   attr_accessor :customer_name
 
@@ -9,13 +5,3 @@ class Customer
     @customer_name = customer_name
   end
 end
-
-new_movie =       Movie.new(title_for_movie: "Jaws", price_code: Movie::NEW_RELEASE)
-kids_movie =      Movie.new(title_for_movie: "Incredibles 2", price_code: Movie::CHILDRENS_MOVIE)
-regular_movie =   Movie.new(title_for_movie: "Avengers", price_code: Movie::REGULAR)
-new_rental =      Rental.new(movie: new_movie, days_rented: 3)
-kids_rental =     Rental.new(movie: kids_movie, days_rented: 3)
-regular_rental =  Rental.new(movie: regular_movie, days_rented: 3)
-customer =        Customer.new(customer_name: "Robert")
-
-puts Interactors::SetPriceAndBonusPoints.call([new_rental, kids_rental, regular_rental], customer)
--- spec/set_price_and_bonus_points_spec.rb
+++ spec/set_price_and_bonus_points_spec.rb
@@ -20,41 +20,24 @@ RSpec.describe Interactors::SetPriceAndBonusPoints do
     end
 
     it 'can calculate a price and bonus points awarded for a new release movie rented for 2 days' do
-      expect(Interactors::SetPriceAndBonusPoints.call([new_rental], customer)).to eq "Rental record for Robert\nAmount owed is $6\nYou earned 2 frequent renter points"
+      expect(Interactors::SetPriceAndBonusPoints.call([new_rental], customer)).to eq "Rental record for Robert\n\tJaws\t$6\nAmount owed is $6\nYou earned 2 frequent renter points"
     end
 
     it 'can calculate a price and bonus points awarded for a kids movie rented for 4 days' do
-      expect(Interactors::SetPriceAndBonusPoints.call([kids_rental], customer)).to eq "Rental record for Robert\nAmount owed is $1.5\nYou earned 1 frequent renter points"
+      expect(Interactors::SetPriceAndBonusPoints.call([kids_rental], customer)).to eq "Rental record for Robert\n\tIncredibles 2\t$1.5\nAmount owed is $1.5\nYou earned 1 frequent renter points"
     end
 
     it 'can calculate a price and bonus points awarded for a kids movie rented for 2 days, and it should cost just as much as for 4 days' do
       new_kids_rental = Rental.new(movie: kids_movie, days_rented: 1)
-      expect(Interactors::SetPriceAndBonusPoints.call([new_kids_rental], customer)).to eq "Rental record for Robert\nAmount owed is $1.5\nYou earned 1 frequent renter points"
+      expect(Interactors::SetPriceAndBonusPoints.call([new_kids_rental], customer)).to eq "Rental record for Robert\n\tIncredibles 2\t$1.5\nAmount owed is $1.5\nYou earned 1 frequent renter points"
     end
 
     it 'can calculate a price and bonus points awarded for a regular movie rented for 2 days' do
-      expect(Interactors::SetPriceAndBonusPoints.call([regular_rental], customer)).to eq "Rental record for Robert\nAmount owed is $2\nYou earned 1 frequent renter points"
+      expect(Interactors::SetPriceAndBonusPoints.call([regular_rental], customer)).to eq "Rental record for Robert\n\tAvengers\t$2\nAmount owed is $2\nYou earned 1 frequent renter points"
     end
 
     it 'can calculate multiple rental bonus points and a prices added up' do
-      expect(Interactors::SetPriceAndBonusPoints.call([new_rental, kids_rental, regular_rental], customer)).to eq "Rental record for Robert\nAmount owed is $9.5\nYou earned 4 frequent renter points"
+      expect(Interactors::SetPriceAndBonusPoints.call([new_rental, kids_rental, regular_rental], customer)).to eq "Rental record for Robert\n\tJaws\t$6\n\tIncredibles 2\t$1.5\n\tAvengers\t$2\nAmount owed is $9.5\nYou earned 4 frequent renter points"
     end
   end
 end
-
-# We have code comments
-# Now for those of you that are new to my blog
-# or are simply curious as to why I am so opposed to comments
-# the reason is twofold and boils down to this:
-#
-# The existence of code comments is proof of hard to understand code
-# and hard to understand code logic.
-# If you need to add comments to your code
-# to make it readable, chances are you need to refactor more
-#
-# And secondly, I have yet to meet a developer that once tasked with
-# making changes to commented code will take the time to
-# improve and change the comments so that they now reflect the new
-# code reality. So what happens is that by the time the third developer
-# come up to the code, the comments say one story because they are outdated
-# and the code tells a whole other story
